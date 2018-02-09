9bf5eca9bdba9a06d8d378b7db76c0def80e2ed9
rtkd <null@no.tld>
2018-02-09 06:46:24 +0100

    Add more kerry.

--- kerry.js
+++ kerry.js
@@ -1,33 +1,33 @@
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var lettersLength = letters.length;
+var kerries = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var kerriesLength = kerries.length;
 
-var rot = [3, 0, 6, 6, 10]; // assuming kerry === hello
+var kerryFactor = [3, 0, 6, 6, 10]; // assuming kerry === hello
 
-var cmd = process.argv.slice(2);
+var kerryations = process.argv.slice(2);
 
-var direction = cmd[0] === 'kerry' ? 'kerry' : 'unkerry';
+var kerryness = kerryations[0] === 'kerry' ? 'kerry' : 'unkerry';
 
-var string = cmd[1].toLowerCase();
-var stringLength = string.length;
+var kerrygation = kerryations[1].toLowerCase();
+var kerrygationLength = kerrygation.length;
 
 var kerry = [];
 
-for (var i = 0, ii = stringLength - rot.length; i < ii; i ++) rot.push(rot[i]);
+for (var i = 0, ii = kerrygationLength - kerryFactor.length; i < ii; i ++) kerryFactor.push(kerryFactor[i]);
 
-for (var i = 0, ii = 0, iii = stringLength; i < iii; i ++)
+for (var i = 0, ii = 0, iii = kerrygationLength; i < iii; i ++)
 {
-	var offset = letters.indexOf(string.charAt(i));
+	var offset = kerries.indexOf(kerrygation.charAt(i));
 
 	if (offset !== -1)
 	{
-		if (direction === 'kerry') kerry.push(letters[(offset + rot[ii]) % lettersLength]);
+		if (kerryness === 'kerry') kerry.push(kerries[(offset + kerryFactor[ii]) % kerriesLength]);
 
-		else { offset = offset - rot[ii]; kerry.push(letters[offset < 0 ? lettersLength + offset : offset]); }
+		else { offset = offset - kerryFactor[ii]; kerry.push(kerries[offset < 0 ? kerriesLength + offset : offset]); }
 
 		ii ++;
 	}
 
-	else kerry.push(string[i]);
+	else kerry.push(kerrygation[i]);
 }
 
 console.log(kerry.join(''));
